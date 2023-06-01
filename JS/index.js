736d521ee78bc786b5108f1fc9eb5f5f71da8779
KnightDestroy <Jhonrey55@gmail.com>
2023-06-01 11:09:02 -0500

    act

---
+++ JS/index.js
@@ -0,0 +1,19 @@
+let desafio = "30 días de JavaScript"
+console.log(desafio)
+console.log(desafio.length)
+console.log(desafio.toUpperCase())
+console.log(desafio.toLowerCase())
+console.log(desafio.substring(3,7))
+console.log(desafio.substring(3,21))
+console.log(desafio.includes("Script"))
+console.log(desafio.split())
+console.log(desafio.split(" "))
+
+let empresas ='Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(empresas.split())
+console.log(empresas.split(","))
+
+console.log(desafio.replace("JavaScript", "Python"))
+console.log(desafio.charAt(15))
+console.log(desafio.charCodeAt(11))
+console.log(desafio.indexOf("a"))
