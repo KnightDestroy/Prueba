6aba6b02f7f4798f615b8dc5937d4904a68e6ec3
KnightDestroy <Jhonrey55@gmail.com>
2023-06-01 12:05:15 -0500

    changes

--- JS/index.js
+++ JS/index.js
@@ -17,3 +17,9 @@ console.log(desafio.replace("JavaScript", "Python"))
 console.log(desafio.charAt(15))
 console.log(desafio.charCodeAt(11))
 console.log(desafio.indexOf("a"))
+console.log(desafio.lastIndexOf("a"))
+
+let oracion = 'No puedes terminar una oración con porque porque porque es una conjunción'
+console.log(oracion.indexOf('porque'))
+console.log(oracion.lastIndexOf('porque'))
+console.log(oracion.search('porque'))
