91767387114e10470597597da6d2e5e906428398
KnightDestroy <Jhonrey55@gmail.com>
2023-06-02 09:14:27 -0500

    actuc

--- JS/index.js
+++ JS/index.js
@@ -16,10 +16,22 @@ console.log(empresas.split(",")) //Separar valores de la matriz en comas
 console.log(desafio.replace("JavaScript", "Python")) //Reemplazar caracteres en la variable
 console.log(desafio.charAt(15)) //Indicar el caracter que se encuentra en la posicion indicada
 console.log(desafio.charCodeAt(11)) //Muestre el codigo Ascii del caracter en la posicion indicada
-console.log(desafio.indexOf("a")) //Muestra la posicion de la primera aparicion del caracter
-console.log(desafio.lastIndexOf("a")) //Muestra la posicion de la ultima aparicion del caracter
+console.log(desafio.indexOf("a")) //Muestra la posicion de la primera aparicion del caracter o cadena
+console.log(desafio.lastIndexOf("a")) //Muestra la posicion de la ultima aparicion del caracter o cadena
 
 let oracion = 'No puedes terminar una oración con porque porque porque es una conjunción'
 console.log(oracion.indexOf('porque')) 
 console.log(oracion.lastIndexOf('porque'))
-console.log(oracion.search('porque')) //Buscar la primera aparicion del caracter solicitado
+console.log(oracion.search('porque')) //Buscar la primera aparicion del caracter o cadena solicitado
+
+let desafio2 = "  30 días de JavaScript  "
+console.log(desafio2)
+console.log(desafio.trim(" ")) //Elimina espacios al principo y final de una cadena
+console.log(desafio.startsWith("30")) //toma una subcadena como argumento y verifica si la cadena comienza con esa subcadena especificada. Devuelve un valor booleano (verdadero o falso).
+console.log(desafio.endsWith("Script")) //toma una subcadena como argumento y verifica si la cadena finaliza con esa subcadena especificada. Devuelve un valor booleano (verdadero o falso).
+console.log(desafio.match("a")) //toma una subcadena o un patrón de expresión regular como argumento y devuelve una matriz si hay una coincidencia; de lo contrario, devuelve un valor nulo.
+
+let desafio3 = "30 días de"
+let desafio4 = " JavaScript"
+console.log(desafio3.concat(desafio4))
+console.log(desafio2.repeat(2))
